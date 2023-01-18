6351ab8e2a0f37fdd56609f5f1ed683061b28896
Ravi Dangi <ravidangi@zibtek.in>
2023-01-18 22:34:15 +0530

    Added Entityframework Package

---
+++ Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieMania.Models;
+using System.Diagnostics;
+
+namespace MovieMania.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
