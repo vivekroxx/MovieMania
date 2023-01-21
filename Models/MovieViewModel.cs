bfd50cd4a4f5474b103932ae5671e62e3bcdfaee
Ravi Dangi <ravidangi@zibtek.in>
2023-01-21 17:43:18 +0530

    Added Minified CDN

---
+++ Models/MovieViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieMania.Models
+{
+    public class MovieViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public TimeSpan Duration { get; set; }
+        public IFormFile Album { get; set; }
+    }
+
+    public class MovieEditForm
+    {
+        public int Id { get; set; }
+    }
+}
