c27b49e6f8e742a8789ca10ae66b401af6e500ff
Ravi Dangi <ravidangi@zibtek.in>
2023-01-23 22:45:01 +0530

    Implemented Global 404 error handling

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieMania.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Requested resource not found";
+                    if (statusCodeResult != null)
+                    {
+                        ViewBag.ErrorPath = statusCodeResult.OriginalPath;
+                        ViewBag.ErrorQS = statusCodeResult.OriginalQueryString;
+                    }
+                    break;
+            }
+
+            return View("NotFound");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,15 +12,15 @@ builder.Services.AddDbContextPool<ApplicationDbContext>(x => x.UseSqlServer(buil
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
-    app.UseStatusCodePages();
+    app.UseStatusCodePagesWithReExecute("/Error/{0}");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
